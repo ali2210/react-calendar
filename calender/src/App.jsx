a8628587387e1435a0bca31076188b4b07bbb22c
Ali2210 <Alideveloper95@gmail.com>
2024-02-14 17:27:29 +0500

    use countdown-react renderer

--- calender/src/App.jsx
+++ calender/src/App.jsx
@@ -7,29 +7,22 @@ import './App.css'
 function App() {
 
   const date = new Date('2024-02-15T23:59:59');
-  const [futuredate, setFuturedate] = useState(date.getUTCDate());
-  const [futuremonth, setFuturemonth] = useState(2);
-  const [futureyear, setFutureyear] = useState(date.getUTCFullYear());
-  const [futurehr, setFuturehr] = useState(date.getUTCHours());
-  const [futuremin, setFuturemin] = useState(date.getUTCMinutes());
-
-
-  
-  const days = date.getDate() - new Date().getDate();
+  const [day, setDay] = useState(0);
   const [hour, setHour] = useState(0);
-  const [min, setMin] = useState(0);
+  const [minute, setMin] = useState(0);
 
+  const renderer = ({days, hours, minutes, completed}) =>{
 
-  useEffect(() =>{
+    if(completed){
 
-    setInterval(() =>{
-
-      setMin(date.getMinutes()- new Date().getMinutes());
-      // console.log("minutes", date.getMinutes()- new Date().getMinutes());
-      setHour(date.getHours() - new Date().getHours());
-    });
-
-  },[]);
+      return <span>Congrats timer stopped here </span>;
+    }else{
+      
+      setDay(days);
+      setHour(hours);
+      setMin(minutes);
+    }
+  }
 
 
 
@@ -38,7 +31,7 @@ function App() {
     <Box>
       <BImage></BImage>
       <Box sx={{color : 'white', position : 'relative', top : '-35pc', left : '25pc', border : 1, borderRadius : 2, width : '7pc', height : '7pc', fontSize : '420%'}}>
-          {days}
+          {day}
       </Box>
       <Box sx={{color : 'white', position : 'relative', top : '-34pc', left : '-12pc', fontSize : '200%'}}>
         Days
@@ -50,16 +43,16 @@ function App() {
         Hours
       </Box>
       <Box sx={{color : 'white', position : 'relative', top : '-55pc', left : '45pc', border : 1, borderRadius : 2, width : '7pc', height : '7pc', fontSize : '420%'}}>
-        {min}
+        {minute}
       </Box>
       <Box sx={{color : 'white', position : 'relative', top : '-54pc', left : '8.2pc', fontSize : '200%'}}>
         Minutes
       </Box>
       <Box sx={{position : 'relative', top : '-53pc', fontSize : '200%', left : '-5pc', color : 'white'}}>
       
-          {futuredate} - {futuremonth} - {futureyear} {futurehr} : {futuremin}
-          <Box visibility={'hidden'}>
-            <Countdown  date={date}></Countdown>
+          {/* {futuredate} - {futuremonth} - {futureyear} {futurehr} : {futuremin} */}
+          <Box visibility={'visible'}>
+            <Countdown  date={date} renderer={renderer}></Countdown>
           </Box>        
       </Box>
     </Box>
